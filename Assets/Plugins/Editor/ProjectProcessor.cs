fde56f9a84bc5047bf51fbb2795f1b67ddd30ef2
xiaoyi <xiaoyi@xd.com>
2021-03-02 17:11:27 +0800

    feat:更新TDSG IOS framework

--- Assets/Plugins/Editor/ProjectProcessor.cs
+++ Assets/Plugins/Editor/ProjectProcessor.cs
@@ -13,7 +13,7 @@ using UnityEngine;
     {
 #if UNITY_IOS
         // 添加标签，unity导出工程后自动执行该函数
-        [PostProcessBuild(100)]
+        [PostProcessBuild]
         /* 
             2020-11-20 Jiang Jiahao
             该脚本中参数为DEMO参数，项目组根据实际参数修改
@@ -44,12 +44,16 @@ using UnityEngine;
                 }
 
                 // Swift编译选项
-                proj.SetBuildProperty(target, "CODE_SIGN_IDENTITY", "iPhone Distribution: Shanghai Xindong Enterprise Development Co., Ltd.");
-                proj.SetBuildProperty(target, "PROVISIONING_PROFILE_SPECIFIER", "Everything 2020");
-                proj.SetBuildProperty(target, "PROVISIONING_PROFILE", "6a542e15-b177-4e10-a884-31e7c51c4857");
-                proj.SetBuildProperty(target, "CODE_SIGN_IDENTITY[sdk=iphoneos*]", "iPhone Distribution: Shanghai Xindong Enterprise Development Co., Ltd.");
+                proj.SetBuildProperty(target, "CODE_SIGN_IDENTITY", "iPhone Developer: GU YUNZE (QNV4UFK7C2)");
+                proj.SetBuildProperty(target, "PROVISIONING_PROFILE_SPECIFIER", "XDSDKDemo_Dev");
+                proj.SetBuildProperty(target, "PROVISIONING_PROFILE", "e3b9cdf8-b425-4c00-aa8c-ba4d7982e662");
+                proj.SetBuildProperty(target, "CODE_SIGN_IDENTITY[sdk=iphoneos*]", "iPhone Developer: GU YUNZE (QNV4UFK7C2)");
 
                 proj.SetBuildProperty(unityFrameworkTarget, "CODE_SIGN_STYLE", "Manual");
+                proj.SetBuildProperty(target, "DEVELOPMENT_TEAM", "NTC4BJ542G");
+
+                proj.SetBuildProperty(target, "PRODUCT_BUNDLE_IDENTIFIER", "com.xd.sdkdemo1");
+
 
                 // rewrite to file  
                 File.WriteAllText(projPath, proj.WriteToString());
